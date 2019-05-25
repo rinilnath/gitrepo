953fd86b51571b757e604d1070625ed3fe0026c8
rinilnath <rinilnath@gmail.com>
2019-05-25 14:33:44 +0530

    Day17-exception propagation

---
+++ src/com/rinil/hackerrank/dayseventeen/Calculator.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package com.rinil.hackerrank.dayseventeen;
+
+/**
+ * @author rinilnath
+ *
+ */
+
---
+++ src/com/rinil/hackerrank/dayseventeen/Solution.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.rinil.hackerrank.dayseventeen;
+
+/**
+ * Program to know exception propagation
+ * @author rinilnath
+ *
+ */
+import java.util.Scanner;
+
+class Calculator {
+
+	public int power(int n, int p) throws Exception {
+		try {
+			if (n < 0 || p < 0) {
+				throw new Exception("n and p should be non-negative");
+			}
+			if (p == 0) {
+				return 1;
+			}
+			if (n == 0) {
+				return 0;
+			}
+			Double t = (double) n;
+			for (int i = 1; i < p; i++) {
+
+				t = t * n;
+
+			}
+
+			return t.intValue();
+		} catch (ArithmeticException e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+}
+
+class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+
+			int n = in.nextInt();
+			int p = in.nextInt();
+			Calculator myCalculator = new Calculator();
+			try {
+				int ans = myCalculator.power(n, p);
+				System.out.println(ans);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		in.close();
+	}
+}
